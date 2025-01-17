ad4722384bf248659d9c8a81d88a3f7b6462739a
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-01-17 18:01:18 -0500

    Feat: Adicionar componente de navegação
    - Sidebar com estilos e integração no layout

---
+++ frontend/src/app/components/template/nav/nav.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: false,
+
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
