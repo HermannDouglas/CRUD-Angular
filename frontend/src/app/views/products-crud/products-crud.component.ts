3b0be9cd5deee04c1563685001c0070ea584c11c
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-01-17 20:28:42 -0500

    Feat: Adicionar rotas para Home e Products CRUD, incluindo novos componentes

---
+++ frontend/src/app/views/products-crud/products-crud.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-crud',
+  standalone: false,
+  
+  templateUrl: './products-crud.component.html',
+  styleUrl: './products-crud.component.css'
+})
+export class ProductsCrudComponent {
+
+}
