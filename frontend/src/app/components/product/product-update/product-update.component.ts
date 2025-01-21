41fcd7a27c392f49670014a509b86a0c57d0d0d9
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-01-21 18:13:18 -0500

    Feat: Implementar funcionalidade de atualização de produtos com formulário e lógica de serviço

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model'; // Importe o modelo de produto
 
 @Component({
   selector: 'app-product-update',
   standalone: false,
-  
   templateUrl: './product-update.component.html',
-  styleUrl: './product-update.component.css'
+  styleUrls: ['./product-update.component.css']
 })
-export class ProductUpdateComponent {
+export class ProductUpdateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: 0
+  };
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id !== null) {
+      this.productService.readById(id).subscribe(product => {
+        this.product = product;
+      });
+    } else {
+      // Lidar com o caso em que o id é null, se necessário
+      console.error('ID do produto não encontrado!');
+    }
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!');
+      this.router.navigate(['/products']);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
+  }
 }
